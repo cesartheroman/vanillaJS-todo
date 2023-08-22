c58d309843899cbb566856ff34154f1497917975
Cesar Roman <cesardroman18@gmail.com>
2023-08-22 15:51:46 -0500

    deleted comments

--- src/services/API.js
+++ src/services/API.js
@@ -9,11 +9,3 @@ export async function fetchTodos() {
     return null;
   }
 }
-
-//TODO: fetchTodos should be testable by itself outside object
-//maybe url is a constant
-//1st: Test that fetch is called with the correct URL.
-//2nd: Make sure data that we're getting from response is stringified
-//return is string and on client side test that it's parsed
-//TODO: refactor this object for more modular
-//Resuse todos for example for reuse in other tests or test files.
